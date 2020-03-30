75d9b1832045f00bb185febe55cd99a033623038
Alexa Jakob <50972270+wolframalexa@users.noreply.github.com>
2020-03-30 11:34:43 -0400

    guy's suggestions

--- Jakob_Assignment_5.m
+++ Jakob_Assignment_5.m
@@ -6,7 +6,7 @@ format short
 % ECE210-A: MATLAB Seminar, Spring 2020
 % Week 5 Homework: 3/25/2020
 
-%% OOP
+%% OOP: creating a cell array of flowers!
 load fisheriris
 
 for i = 1:150
@@ -27,17 +27,16 @@ report(flowers{1}) % example of using report
 
 
 %% Bonus Points
-% 2
-r = exp(pi*sqrt(163));
-r_sym = sym(r);
-r_30 = vpa(r_sym,30);
-r_100 = vpa(r_sym,100);
+% 2: comparing precision values
+r = sym(exp(pi*sym(sqrt(163))));
+r_30 = vpa(r,30);
+r_100 = vpa(r,100);
 
 table(r, r_30, r_100)
 
-%% 3
+%% 3: how often digits appear in pi
 vpaPi = vpa(pi,10001);
-strPi = sym2cell(vpaPi);
+strPi = char(vpaPi);
 
 digits = zeros(10,1);
 for i = 1:10001
