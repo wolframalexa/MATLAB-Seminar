c60d6356e55a98269e7769aaa82f94e8074c9640
Alexa Jakob <50972270+wolframalexa@users.noreply.github.com>
2020-05-05 11:20:23 -0400

    fix AR model coeffs, walk generation

--- Week9/Jakob_Assignment_9.m
+++ Week9/Jakob_Assignment_9.m
@@ -13,45 +13,42 @@ n = 1:size(goog,1);
 plot(n,goog)
 
 sys = ar(goog,10,'ls');
-coeffs = sys.A;
+coeffs = -1*sys.A(2:end);
     
 last = size(goog,1);
 goog = [goog; zeros(10,1)];
 for n = 1:10
     temp = goog(last+n-10:last+n-1);
-    goog(last+n) = sum(sys.D + sys.NoiseVariance + sum(coeffs.*temp));
+    goog(last+n) = sys.D + sum(coeffs.*temp');
 end
 
-% that's a lot higher than I would expect given the previous values
 
 %% Walking
-walks = ones(50,300);
-walks = -walks;
-temp = 2*randi([0 1],50,300);
-walks = walks + temp;
+walks = zeros(50,300);
+for i = 1:50
+    current = 0;
+    for j = 1:300
+        walks(i,j) = current + randsrc;
+        current = walks(i,j);
+    end
+end
 
 figure
-n = 1:size(walks,2);
-for i = 1:size(walks,1)
-    plot(n,walks(i,:))
-    hold on
-end
+plot(walks)
 title('Equal Probability Walks')
 
-pmf = [0.25, 0.75];
-population = [-1 1];
-sample_size = 300;
 walks2 = zeros(50,300);
-
-for i=1:50
-    walks2(i,:) = randsample(population,sample_size,true,pmf);
+for i = 1:50
+    current = 0;
+    for j = 1:300
+        walks2(i,j) = current + randsrc(1,1,[-1 1; 0.25 0.75]);
+        current = walks2(i,j);
+    end
 end
 
 figure
-for i = 1:size(walks2,1)
-    plot(n,walks2(i,:))
-    hold on
-end
+plot(walks2)
 title('Weighted towards 1 (75%)')
-% more spacing between points
+
+% upper half-plane, whereas equal goes up and down
 
